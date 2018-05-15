b287a50007ddee2a5af6e51e251372c0a87eda21
alidus <faystain@mail.ru>
2018-05-16 00:41:03 +0500

    Создана базовая структура НН, готова функция обучения

--- main.py
+++ main.py
@@ -7,49 +7,47 @@ import torch.functional as F
 from torch.autograd import Variable
 
 
-
-
-def load_data(filename):
-    with open(filename) as f:
+def load_json_data():
+    with open("ratesLearn.json") as f:
         learning_data = np.transpose(np.asarray(json.load(f)["dataset"]["data"]))
+        timesteps = np.asarray(learning_data[0])
+        dataset = normalize_array(np.asarray(learning_data[1], float))
 
-    x_data = learning_data[0]
-    y_data = np.asarray(learning_data[1], float)
-    y_data = normalize_array(y_data)
-    return x_data, y_data
+        return dataset, timesteps
 
+
+def prepare_dataset(dataset):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - 1):
+        a = dataset[i]
+        dataX.append(a)
+        dataY.append(dataset[i+1])
+    a = dataset[len(dataset) - 1]
+    dataX.append(a)
+    dataY.append(dataset[0])
+    return np.array(dataX), np.array(dataY)
 
 
 class LSTMNet(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, dropout=0):
+    def __init__(self, input_size, hidden_size, seq_len, batch_size):
         super(LSTMNet, self).__init__()
-
+        self.batch_size = batch_size
         self.hidden_size = hidden_size
-        self.inp = torch.nn.Linear(hidden_size, hidden_size)
-        self.rnn = torch.nn.LSTM(input_size=input_size, hidden_size = hidden_size, num_layers=1, dropout=dropout,
-                                 bidirectional=False)
-        self.out = torch.nn.Linear(hidden_size, 1)
-        self.relu = torch.nn.ReLU
-
-    def step(self, input, hidden=None):
-        input = self.inp(input.view(1, -1)).unsqueeze(1)
-        output, hidden = self.rnn(input, hidden)
-        output = self.out(output.squeeze(1))
-        return output, hidden
+        self.seq_len = seq_len
+        self.lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=1)
+        self.output = torch.nn.Linear(hidden_size, 1)
+        self.hidden = self.init_hidden()
+        self.cell = self.init_hidden()
 
     def forward(self, x):
-        batch_size = x.size()[0]
-        seq_len = x.size()[1]
+        x = x.view(self.seq_len, self.batch_size, 1)
+        output, (self.hidden, self.cell) = self.lstm(x, (self.hidden, self.cell))
+        output = self.output(output)
+        return output
 
-        x = x.view(seq_len, batch_size, -1)
-        h0 = Variable(torch.zeros(seq_len, batch_size, self.hidden_size))
-        c0 = Variable(torch.zeros(seq_len, batch_size, self.hidden_size))
-        outputs, (ht, ct) = self.rnn(x, (h0, c0))
-        out = outputs[-1]
+    def init_hidden(self):
+        return Variable(torch.zeros(1, self.batch_size, self.hidden_size).double().cuda())
 
-        out = self.out(out.view(len(x), -1))
-        out = self.relu(out)
-        return out
 
 def normalize_array(array):
     max_el = max(array)
@@ -58,29 +56,79 @@ def normalize_array(array):
         array[i] = (array[i] - min_el) / (max_el - min_el)
     return array
 
-n_epochs = 10
-n_iters = 50
 
-model = LSTMNet(1, 5)
+def convert_cuda_tesor_to_1dim_ndarray(tensor):
+    return tensor.cpu().numpy().reshape(100)
+
+def train_model(model, n_epochs, input_size, hidden_size, seq_len, batch_size, trainX, trainY):
+    predict = []
+    for epoch in range(n_epochs):
+        for timestep in range(len(trainX) - 1):
+            model.zero_grad()
+            model.hidden = model.init_hidden()
+            model.cell = model.init_hidden()
+            predict = model(trainX)
+            losses = criterion(predict, trainY)
+            losses.backward()
+            optimizer.step()
+        print("EPOCH: " + str(epoch))
+
+def give_prediction(model, num_of_regions):
+    with torch.no_grad():
+        for timestep in range(len(trainX) - 1):
+            model.hidden = model.init_hidden()
+            model.cell = model.init_hidden()
+            predict = model(trainX)
+            losses = criterion(predict, trainY)
+            losses.backward()
+            optimizer.step()
+        print("EPOCH: " + str(epoch))
+
+
+n_epochs = 500
+input_size = 1
+hidden_size = 100
+seq_len = 50
+batch_size = 2
+
+json_data_array = load_json_data()
+timesteps = json_data_array[1]
+trainX, trainY = prepare_dataset(json_data_array[0][0:100])
+testX, testY = prepare_dataset(json_data_array[0][100:200])
+
+trainX = torch.from_numpy(np.reshape(trainX, (seq_len, batch_size, 1)))
+testX = torch.from_numpy(np.reshape(testX, (seq_len, batch_size, 1)))
+
+trainY = torch.from_numpy(np.reshape(trainY, (seq_len, batch_size, 1)))
+testY = torch.from_numpy(np.reshape(testY, (seq_len, batch_size, 1)))
+
+trainX = trainX.cuda()
+trainY = trainY.cuda()
+testX = trainX.cuda()
+testY = trainY.cuda()
+
+model = LSTMNet(input_size, hidden_size, seq_len, batch_size)
+model.double()
+model.cuda()
 criterion = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
 
-losses = np.zeros(n_epochs)
-data = load_data("ratesLearn.json")
+fig = plt.figure(figsize=(10, 5))
+ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan=40, colspan=40)
+ax1.plot(timesteps[0:100], convert_cuda_tesor_to_1dim_ndarray(trainY), color="grey", label="original rates")
 
-with torch.no_grad():
-    inputs = torch.from_numpy(data[1]).view(1, -1)
-    print(model(inputs))
-    pass
+train_model(model, n_epochs, input_size, hidden_size, seq_len, batch_size, trainX, trainY)
+result = give_prediction(model)
 
 
-# fig = plt.figure(figsize=(10, 5))
-#     ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan=40, colspan=40)
-#     ax1.plot(x_data, y_data, color="grey", label="original rates")
-#     ax1.xaxis.set_major_locator(ticker.MultipleLocator(10))
-#     plt.xticks(rotation=45)
-#     plt.subplots_adjust(bottom=.20)
-#     plt.show()
+ax1.plot(timesteps[0:100], convert_cuda_tesor_to_1dim_ndarray(predict.detach()), color="blue", label="prediction")
+trainX = convert_cuda_tesor_to_1dim_ndarray(trainX)
+trainY = convert_cuda_tesor_to_1dim_ndarray(trainY)
+
+ax1.xaxis.set_major_locator(ticker.MultipleLocator(10))
+plt.xticks(rotation=45)
+plt.subplots_adjust(bottom=.20)
+plt.show()
 
 #
 # if torch.cuda.is_available():
