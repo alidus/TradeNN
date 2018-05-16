106d8ee3e80dd07d17c179c919174e90c6f542b2
alidus <faystain@mail.ru>
2018-05-17 01:09:58 +0500

    Изменения в структуре кода, начата работа по разделению обучения/предсказания на циклы

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import matplotlib.ticker as ticker
 import torch.functional as F
 from torch.autograd import Variable
 
+n_epochs = 20
+input_size = 1
+hidden_size = 100
+batch_size = 4
+n_regions = 5
+total_length = 200
+steps_in_region = total_length // n_regions
+seq_len = steps_in_region // batch_size
 
 def load_json_data():
     with open("ratesLearn.json") as f:
@@ -58,72 +66,70 @@ def normalize_array(array):
 
 
 def convert_cuda_tesor_to_1dim_ndarray(tensor):
-    return tensor.cpu().numpy().reshape(100)
+    return tensor.cpu().numpy().reshape(total_length)
 
-def train_model(model, n_epochs, input_size, hidden_size, seq_len, batch_size, trainX, trainY):
+
+def train_model(model, n_epochs, input, target):
     predict = []
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+    model.cuda().double()
     for epoch in range(n_epochs):
-        for timestep in range(len(trainX) - 1):
+        for timestep in range(len(input) - 1):
             model.zero_grad()
             model.hidden = model.init_hidden()
             model.cell = model.init_hidden()
-            predict = model(trainX)
-            losses = criterion(predict, trainY)
+            predict = model(input)
+            losses = criterion(predict, target)
             losses.backward()
             optimizer.step()
         print("EPOCH: " + str(epoch))
 
-def give_prediction(model, num_of_regions):
+
+def give_prediction(model, prediction_length, starting_x):
+    predictions = []
+    predict = starting_x
     with torch.no_grad():
-        for timestep in range(len(trainX) - 1):
+        for timestep in range(prediction_length - 1):
             model.hidden = model.init_hidden()
             model.cell = model.init_hidden()
-            predict = model(trainX)
-            losses = criterion(predict, trainY)
-            losses.backward()
-            optimizer.step()
-        print("EPOCH: " + str(epoch))
+            predict = model(predict)
+            predictions.append(predict)
+            # losses = criterion(predict, trainY)
+            # losses.backward()
+        return predictions
 
 
-n_epochs = 500
-input_size = 1
-hidden_size = 100
-seq_len = 50
-batch_size = 2
+def prepare_tensors(array_of_inputs, sequence_length = seq_len):
+    teX, teY = prepare_dataset(array_of_inputs)
+    teX = torch.from_numpy(np.reshape(teX, (sequence_length, batch_size, 1))).cuda()
+    teY = torch.from_numpy(np.reshape(teY, (sequence_length, batch_size, 1))).cuda()
+    return teX, teY
+
+
+
 
 json_data_array = load_json_data()
 timesteps = json_data_array[1]
-trainX, trainY = prepare_dataset(json_data_array[0][0:100])
-testX, testY = prepare_dataset(json_data_array[0][100:200])
 
-trainX = torch.from_numpy(np.reshape(trainX, (seq_len, batch_size, 1)))
-testX = torch.from_numpy(np.reshape(testX, (seq_len, batch_size, 1)))
-
-trainY = torch.from_numpy(np.reshape(trainY, (seq_len, batch_size, 1)))
-testY = torch.from_numpy(np.reshape(testY, (seq_len, batch_size, 1)))
-
-trainX = trainX.cuda()
-trainY = trainY.cuda()
-testX = trainX.cuda()
-testY = trainY.cuda()
-
-model = LSTMNet(input_size, hidden_size, seq_len, batch_size)
-model.double()
-model.cuda()
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
-
+testX, testY = prepare_tensors(json_data_array[0][:total_length], total_length // batch_size)
 fig = plt.figure(figsize=(10, 5))
 ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan=40, colspan=40)
-ax1.plot(timesteps[0:100], convert_cuda_tesor_to_1dim_ndarray(trainY), color="grey", label="original rates")
+# Построить график оригинальных цен
+ax1.plot(timesteps[0:200], convert_cuda_tesor_to_1dim_ndarray(testY), color="grey", label="original rates")
 
-train_model(model, n_epochs, input_size, hidden_size, seq_len, batch_size, trainX, trainY)
-result = give_prediction(model)
+model = LSTMNet(input_size, hidden_size, seq_len, batch_size)
+
+
+# Основной цикл
+for i in range(n_regions):
+    subseq = json_data_array[0][i * steps_in_region : (i+1) * steps_in_region]
+    testX, testY = prepare_tensors(subseq)
+    train_model(model, n_epochs, testX, testY)
+    result = give_prediction(model, steps_in_region, json_data_array[0][(i+1) * steps_in_region])
 
 
 ax1.plot(timesteps[0:100], convert_cuda_tesor_to_1dim_ndarray(predict.detach()), color="blue", label="prediction")
-trainX = convert_cuda_tesor_to_1dim_ndarray(trainX)
-trainY = convert_cuda_tesor_to_1dim_ndarray(trainY)
 
 ax1.xaxis.set_major_locator(ticker.MultipleLocator(10))
 plt.xticks(rotation=45)
